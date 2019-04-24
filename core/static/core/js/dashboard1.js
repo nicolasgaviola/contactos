d29812c40d15aa77772bac8be07fb3404c222249
Nicolás Gaviola <nicolasgaviola@gmail.com>
2019-04-24 12:38:15 -0300

    Cambios en archivos y agregado de app contacto

--- core/static/core/js/dashboard1.js
+++ core/static/core/js/dashboard1.js
@@ -142,14 +142,14 @@ $(function () {
     // ============================================================== 
     // This is for the popup message while page load
     // ============================================================== 
-        $.toast({
-            heading: 'Welcome to Adminpro',
-            text: 'Most powerfull and elegant design with tons of elements.',
-            position: 'top-right',
-            loaderBg: '#f33c49',
-            icon: 'info',
-            hideAfter: 6000,
-            stack: 6
-        })
+        // $.toast({
+        //     heading: 'Welcome to Adminpro',
+        //     text: 'Most powerfull and elegant design with tons of elements.',
+        //     position: 'top-right',
+        //     loaderBg: '#f33c49',
+        //     icon: 'info',
+        //     hideAfter: 6000,
+        //     stack: 6
+        // })
     
 });
